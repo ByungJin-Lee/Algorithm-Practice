d20a8f0482b57745c3641a2450ac05c615881185
ByungJin-Lee <Byungjin.dev@gmail.com>
2021-04-02 13:51:42 +0900

    solving 10757

---
+++ 10757/solv.c
@@ -0,0 +1,45 @@
+/*
+	P_Name : "Big!!! Number"
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	//Variables
+	char *nBuffer_1, *nBuffer_2;
+	int length_1 = 0, length_2 = 0, max, sub;
+	int *result;
+	//Input
+	scanf("%s %s", &nBuffer_1, &nBuffer_2);
+	//Work
+	//Count - length
+	while(nBuffer_1[length_1] != 0) length_1++;
+	while(nBuffer_2[length_2] != 0) length_2++;
+	//Compare
+	if(length_1 >= length_2) {
+		max = length_1; sub = max - length_2;
+	}else{
+		max = length_2; sub = max - length_1;
+		//change postion
+		char *temp = nBuffer_1; nBuffer_1 = nBuffer_2; nBuffer_2 = temp;
+	}
+	result = (int*)malloc(sizeof(int)*max + 1); //Plus One digit
+	//init
+	for(int i = 0; i<max;result[i++] = -1);
+	//Input value
+	for(int i = max; i >= sub; i--)
+		result[i] = (int)((nBuffer_1[i] - '0') + (nBuffer_2[i-sub] - '0')); 
+	for(int i = 0; i < sub; i++)
+	 	result[i+1] = (int)(nBuffer_1[i] - '0');
+	//Calculate
+	for(int i = max; i > 0; i--){
+		if(result[i] >= 10){
+			result[i] -= 10;
+			if(result[i-1] == -1) result[i-1] = 1;
+			else result[i-1]++;
+		}
+	}
+	//Display
+	for(int i = 0; i <= max; i++)
+		if(result[i] != -1) printf("%d",result[i]);
+}
