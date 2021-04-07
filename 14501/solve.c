c3ad0b346e31e749d8224f57b3cbdb3372444bc0
ByungJin-Lee <Byungjin.dev@gmail.com>
2021-04-07 14:30:02 +0900

    solve 14501

--- 14501/solve.c
+++ 14501/solve.c
@@ -4,52 +4,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int search(int *times, int *money, int start, int end){
-	int result = 0;
-	for(int i = start, left, right = 0; i < end; i++){		
-		//If timeSchedule Value is equal "-1", then impossible Schedule
-		if(times[i] == -1) continue;
-		//if 1 then always work
-		if(times[i] == 1){
-			result += money[i];
-			continue;
-		}
-		//Work
-		//not 1 then Recursive
-		//Start This
-		left = search(times, money, i+times[i], end);
-		//Start Another
-		for(int j = i+1, size = i+times[i]-1, temp; j < size; j++){
-			if((temp = search(times, money, j, end)) > right) right = temp;
-		}
-		//Compare
-		result = result + (left > right) ? left:right;
-	}
-	return result;
+int search(int *times, int *money, int s, int end){		
+	//End Condition
+	int sum = 0, left = 0, right = 0;
+	//If timeSchedule Value is equal "-1", then impossible Schedule		
+	//if 1 then always work
+	while(s < end && times[s] == 1) sum = sum + money[s++];		
+	while(s < end && times[s] == -1 ) s++;
+	if(s>=end) return sum;
+	//Work
+	//-choose 2 case
+	//--choose this
+	left = money[s] + search(times, money, s + times[s], end);			
+	//--chose next		
+	for(int i = s + 1, temp; i < times[s] + s; i++)
+		if((temp = search(times, money, i, end)) > right) right = temp;
+	//-calc result
+	if(left > right) sum+=left;
+	else sum+=right;
+	return sum;
 }
 
 int main(void){
 	//Variables
 	int count_set, count_not_contain = 0, totalMoney;
-	int *timeSchedule, *workSchedule;
-	
+	int *timeSchedule, *workSchedule;	
 	//Input
 	scanf("%d", &count_set);
 	//0-create Dynamics Array
 	timeSchedule = (int*)malloc(sizeof(int)*count_set);
 	workSchedule = (int*)malloc(sizeof(int)*count_set);
 	//0-Input Value
-	for(int i = 0; i < count_set; scanf("%d%d", &timeSchedule[i], &workSchedule[i++]));
-	
+	for(int i = 0; i < count_set; scanf("%d%d", &timeSchedule[i], &workSchedule[i]),i++);	
 	//Work
+	//Check Table
 	//0-Check Possible(if impossible time then save -1)
-	for(int i = 0; i < count_set; timeSchedule[i] = (timeSchedule[i]+i > count_set)?-1:timeSchedule[i],i++);
-	//1-Calculate
-	totalMoney = search(timeSchedule, workSchedule, 0, count_set);	
-	
+	for(int i = 0; i < count_set; timeSchedule[i] = (timeSchedule[i]+i > count_set)?-1:timeSchedule[i],i++);		
 	//Display
-	printf("%d", totalMoney);
-	
+	printf("%d", search(timeSchedule, workSchedule, 0, count_set));	
 	//Free
 	free(timeSchedule);
 	free(workSchedule);
