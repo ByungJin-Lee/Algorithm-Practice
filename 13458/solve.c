495e15c56782f9900b7f130b9be21becb9b64b51
ByungJin Lee <Byungjin.dev@gmail.com>
2021-04-15 15:26:27 +0900

    Update solve.c

--- 13458/solve.c
+++ 13458/solve.c
@@ -3,20 +3,28 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(void){
 	//Variables
-	int n_room;
-	int *list_n_student;
-	int c_see_king, c_see_pown;
+	int n_room, c_see_king, c_see_pown;
+	unsigned long long min = 0;
+	int *list_n_student;	
 	//Input
 	scanf("%d", &n_room);
 	list_n_student = (int*)malloc(sizeof(int)*n_room);
-	for(int i = 0, temp = 0; i < n_room; list_n_student[i++] = temp) scanf("%d", &temp);
+	for (int i = 0, temp = 0; i < n_room; list_n_student[i++] = temp) scanf("%d", &temp);
 	scanf("%d%d", &c_see_king, &c_see_pown);
 	//Work
-	
+	for (int i = 0, r_temp = 0; i < n_room; i++, r_temp = 0){
+		
+		r_temp = list_n_student[i] - c_see_king;
+		min++;
+		if (r_temp <= 0) continue;
+		min += (int)ceil(r_temp / (double)c_see_pown);
+	}
 	//Display - min
-	printf("%d");
+	printf("%llu", min);
+	free(list_n_student);
 	return 0;
 }
