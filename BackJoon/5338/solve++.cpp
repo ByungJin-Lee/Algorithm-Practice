8eb7a3fe6a624006defac34d95126c6b589039ec
byungjin <byungjin.dev@gmail.com>
2022-05-11 21:29:29 +0900

    5338

---
+++ BackJoon/5338/solve++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|\n";
+    
+    return 0;
+}
