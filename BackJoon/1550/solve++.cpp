f100b4b4f873d569ec2aa181759f22d34e2caec4
byungjin <byungjin.dev@gmail.com>
2022-05-13 22:38:19 +0900

    1550

---
+++ BackJoon/1550/solve++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void){
+//    ios::sync_with_stdio(false);
+//    cin.tie(0);
+    
+    int value;
+    
+    scanf("%X", &value);
+    
+    cout << value;
+    
+    return 0;
+}
