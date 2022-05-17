5dea4ec09d9895ada906c9563af5f9e404e5e6d3
byungjin <byungjin.dev@gmail.com>
2022-05-17 20:01:20 +0900

    3046

---
+++ BackJoon/3046/solve++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int a, s;
+    
+    cin >> a >> s;
+    
+    cout << 2 * s - a;
+    
+    return 0;
+}
