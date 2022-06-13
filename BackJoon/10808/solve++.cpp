0fcb9d929f6866618496e8f05ef6a43eddb64dc7
byungjin <byungjin.dev@gmail.com>
2022-06-13 23:19:20 +0900

    10808

---
+++ BackJoon/10808/solve++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(void){
+    char word[101];
+    int counter[26];
+    memset(counter, 0, sizeof(counter));
+    
+    cin >> word;
+    
+    int i = 0;
+    while(word[i] != 0){
+        counter[word[i] - 'a']++;
+        i++;
+    }
+    
+    for(i = 0; i < 26; i++) cout << counter[i] << ' ';
+    
+    return 0;
+}
