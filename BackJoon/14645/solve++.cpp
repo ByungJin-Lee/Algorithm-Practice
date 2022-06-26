105682615b580f00b2b6b42459821ef0393f91cc
byungjin <byungjin.dev@gmail.com>
2022-06-26 23:30:14 +0900

    14645

---
+++ BackJoon/14645/solve++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    int b, v;
+    cin >> b >> v;
+    for(int i = 0; i < b; i++) cin >> v >> v;
+    cout<< "비와이";
+    return 0;
+}
