3973f0b3d59c579ec9324169e5870fceb6818e7f
byungjin <byungjin.dev@gmail.com>
2022-07-03 23:10:38 +0900

    20492

---
+++ BackJoon/20492/solve++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    
+    cin >> num;
+    
+    cout << num * 78 / 100 << ' ' << (num * 4 / 5) + (num * 1 / 5) * 78 / 100;
+    
+    return 0;
+}
